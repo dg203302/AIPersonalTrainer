127b17938c7c6bb15987ec4989081fc8a83bae43
Diego García <168243435+dg203302@users.noreply.github.com>
2026-01-02 12:45:49 -0300

    feat: implement Google login functionality with error handling

--- Scripts/index_s.js
+++ Scripts/index_s.js
@@ -1,3 +1,9 @@
 document.getElementById("boton_google").addEventListener("click",async () => {
-    //llamar a la api de google oauth
+    const response = await fetch("/API/Login_google");
+    const result =  await response.json();
+    if(response.ok){
+        window.location.href = result.redirectUrl;
+    } else {
+        alert("Error al iniciar sesión con Google: " + result.error);
+    }
 });
