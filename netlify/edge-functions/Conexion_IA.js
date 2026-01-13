3c9fd3958a309ebc7e2223b7793120701ccd4d97
Diego García <168243435+dg203302@users.noreply.github.com>
2026-01-12 21:36:38 -0300

    refactor: update environment variable retrieval to use neon for API keys

--- netlify/edge-functions/Conexion_IA.js
+++ netlify/edge-functions/Conexion_IA.js
@@ -1,7 +1,8 @@
 import { GoogleGenerativeAI } from "https://esm.sh/@google/generative-ai@0.21.0";
+import { neon } from "https://esm.sh/@neondatabase/serverless";
 
 export default async function handler(request, context){
-	const apiKey = Deno.env.get("Gemini_API_Key")
+	const apiKey = neon(Deno.env.get("Gemini_API_Key"));
 	if (!apiKey) {
 		throw new Error("Missing Gemini_API_Key environment variable");
 	}
--- netlify/edge-functions/Conexion_supabase.js
+++ netlify/edge-functions/Conexion_supabase.js
@@ -1,8 +1,10 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { neon } from "https://esm.sh/@neondatabase/serverless";
+
 
 export default function conectar_supabase() {
-    const Id_bd = Deno.env.Supabase_Api_Key;
-    const Url_bd = Deno.env.Supabase_Project_Url;
+    const Id_bd = neon(Deno.env.get("Supabase_Api_Key"));
+    const Url_bd = Deno.env.get("Supabase_Project_Url");
     const supabase = createClient(Url_bd, Id_bd);
     return { supabase };
 }
