4a78b249fa904a8739155bf08ab9335b50621463
Diego García <168243435+dg203302@users.noreply.github.com>
2026-02-04 21:26:28 -0300

    feat: Update Supabase client import to use ESM version

--- netlify/edge-functions/actualizar_cambios_plan.js
+++ netlify/edge-functions/actualizar_cambios_plan.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 const supabaseUrl = "https://lhecmoeilmhzgxpcetto.supabase.co";
 const supabaseKey = "sb_secret_8pOt21ZHhoru6-VbtV6sEQ_TYL8DivC";
 const supabase = createClient(supabaseUrl, supabaseKey);
